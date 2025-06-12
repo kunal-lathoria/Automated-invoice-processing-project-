56b9f93e7b5eef5d42a28dcf1eeb91c41c7674ae
Kunal Lathoria <lathoriakunal@gmail.com>
2025-06-12 12:15:51 +0530

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,1 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_s3_bucket" "invoice_bucket" {
+  bucket = "invoice-processing-${var.environment}"
+  acl    = "private"
+}
+
+resource "aws_lambda_function" "process_invoice" {
+  filename      = "lambda_function.zip"
+  function_name = "process_invoice"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "index.handler"
+  runtime       = "nodejs14.x"
+  
+  environment {
+    variables = {
+      INVOICE_BUCKET = aws_s3_bucket.invoice_bucket.id
+    }
+  }
+}
